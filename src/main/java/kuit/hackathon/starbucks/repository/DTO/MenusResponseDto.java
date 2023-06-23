28110998fffef24c6934e3b2b1a3ca5d7677526f
kang yeon joo <rkdduswn0409@gmail.com>
2023-06-24 05:52:32 +0900

    modify : 메뉴 리스트에 가격 데이터 추가

--- src/main/java/kuit/hackathon/starbucks/repository/DTO/MenusResponseDto.java
+++ src/main/java/kuit/hackathon/starbucks/repository/DTO/MenusResponseDto.java
@@ -5,6 +5,8 @@ import kuit.hackathon.starbucks.domain.entity.menu.Menu;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.math.BigInteger;
+
 @Getter
 @AllArgsConstructor
 public class MenusResponseDto {
@@ -12,11 +14,13 @@ public class MenusResponseDto {
     private final String name_kr;
     private final String name_eng;
     private final String image_url;
+    private final Long price;
 
     public MenusResponseDto(Menu menu){
         this.id= menu.getId();
         this.name_kr=menu.getName().getNameKr();
         this.name_eng=menu.getName().getNameEng();
         this.image_url=menu.getImageUrl();
+        this.price=menu.getPrice();
     }
 }
