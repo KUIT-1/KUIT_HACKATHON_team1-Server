5239c1f1c2b3b042702a39cb7275b2561ab6bdf8
kang yeon joo <rkdduswn0409@gmail.com>
2023-06-24 03:02:22 +0900

    feat : 세부 카테고리 API 구현

---
+++ src/main/java/kuit/hackathon/starbucks/repository/DTO/CategoryResponseDto.java
@@ -0,0 +1,11 @@
+package kuit.hackathon.starbucks.repository.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CategoryResponseDto {
+    private Long id;
+    private String name;
+}
--- src/main/java/kuit/hackathon/starbucks/repository/SubCategoryRepository.java
+++ src/main/java/kuit/hackathon/starbucks/repository/SubCategoryRepository.java
@@ -2,7 +2,13 @@ package kuit.hackathon.starbucks.repository;
 
 import kuit.hackathon.starbucks.domain.entity.category.SubCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
-
+    
+    @Query(value = "select s from SubCategory s where s.mainCategory.id =:id")
+    List<SubCategory> getAllByMainCategoryId(@Param("id") Long id);
 }
