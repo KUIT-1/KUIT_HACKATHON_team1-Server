a11cfe65add3a5a948e87fc80a45245780d77bca
hhj <gudwn357@gmail.com>
2023-06-23 17:12:12 +0900

    feat (MenuRepository) : create findRecommendMenuByDtype()

--- src/main/java/kuit/hackathon/starbucks/repository/MenuRepository.java
+++ src/main/java/kuit/hackathon/starbucks/repository/MenuRepository.java
@@ -2,7 +2,14 @@ package kuit.hackathon.starbucks.repository;
 
 import kuit.hackathon.starbucks.domain.entity.menu.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
 
+    @Query(value = "select m from Menu m where m.dtype = :dtype")
+    List<Menu> findRecommendMenuByDtype(@Param("dtype") String dType);
 }
+
