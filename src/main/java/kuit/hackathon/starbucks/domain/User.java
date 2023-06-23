9faba8d11c4d939f2cc400975ff9cf81a9c0db7b
hhj <gudwn357@gmail.com>
2023-06-23 11:24:47 +0900

    first commit

---
+++ src/main/java/kuit/hackathon/starbucks/domain/User.java
@@ -0,0 +1,20 @@
+package kuit.hackathon.starbucks.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String name;
+}
