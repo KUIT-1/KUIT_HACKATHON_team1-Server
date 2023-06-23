83c7517a7f9f14d313cd2cb1d54440dd78d681d7
hhj <gudwn357@gmail.com>
2023-06-23 13:49:44 +0900

    feat (StarbucksApplication) : add auditing

--- src/main/java/kuit/hackathon/starbucks/StarbucksApplication.java
+++ src/main/java/kuit/hackathon/starbucks/StarbucksApplication.java
@@ -2,8 +2,10 @@ package kuit.hackathon.starbucks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class StarbucksApplication {
 
 	public static void main(String[] args) {
